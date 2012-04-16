b2647ca971469ff821984d43d342e11f91e32b65
Koop Otten <ottenkoop@live.nl>
2012-04-16 23:28:19 +0200

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /users/1
+  # end /users/1
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
